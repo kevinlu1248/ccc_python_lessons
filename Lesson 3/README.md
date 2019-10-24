f47531ac6ad423646d31e302da513fede8808d88
kevin <kevinlu1248@gmail.com>
2019-10-23 23:29:49 -0700

    comments

--- Lesson 3/README.md
+++ Lesson 3/README.md
@@ -1,6 +1,6 @@
 ## Schedule and Outline
-1. [lambda.py](https://github.com/kevinlu1248/ccc_python_lessons/tree/master/Lesson%203/lambda.py): (5 minutes) Introduce lambda function to them
-2. (Remainder of the time) Try doing https://dmoj.ca/problem/ccc18s2 and https://dmoj.ca/problem/ccc16j2 using the input mechanics discussed!
+1. [lambda.py](https://github.com/kevinlu1248/ccc_python_lessons/tree/master/Lesson%203/lambda.py): (15 minutes) Introduce lambda function to them
+2. (Remainder of the time) Try the listed problems
 
-**Note that each of the Python file's teaching materials can be found in the Lesson 1 repository at:**
-https://github.com/kevinlu1248/ccc_python_lessons/tree/master/Lesson%202
+**Note that each of the Python file's teaching materials can be found in the Lesson 3 repository at:**
+https://github.com/kevinlu1248/ccc_python_lessons/tree/master/Lesson%203
--- Lesson 3/lambda.py
+++ Lesson 3/lambda.py
@@ -20,4 +20,48 @@ print(square('1 2 3 4 5'))
 # furthermore, it can also be used as a shorthand
 
 # so lets first look a simple usage of it to subsitute a function
-test
+# this function squares a string then returns a string
+square_string = lambda i: str(int(i) ** 2)
+print(square_string('5'))
+
+# the beauty of the implementation arises when you simply don't name it and apply it directly
+print((lambda i: str(int(i) ** 2))('5'))
+
+# but can't we just use print(str(int('5') ** 2))?
+# it is significantly more practical when used with map, filter and reduce!
+# as you've seen earlier, map can be used to iterate the function over the entire iterable
+# meanwhile filter does exactly what it sounds like it would do: filter items
+# for example suppose you wanted to filter out any negative numbers
+# you could simply use the following
+
+print(list(filter(lambda i: i >= 0, [-5, -3, 2, 6])))
+
+# this is equivalent to
+output = []
+for i in [-5, -3, 2, 6]:
+    if i >= 0:
+        output.append(i)
+print(output)
+
+# the last one is simply reduce, which iteratively sets the first term to the function of the first two terms
+# eg the following produces the sum
+
+from functools import reduce
+print(reduce((lambda a, b: a + b), [-5, -3, 2, 6]))
+
+# this is equivalent to
+sum = 0
+for i in [-5, -3, 2, 6]:
+    sum += i
+print(sum)
+
+# ACTIVITY: make an equivalent product function (there's no builtin product function for this)
+
+# try the following problems:
+# https://dmoj.ca/problem/ccc18j3
+# https://dmoj.ca/problem/ccc15j3 (really long question)
+# https://dmoj.ca/problem/ccc14s2 (hint: use tuples)
+
+# code golf (shortest solution wins!)
+# https://dmoj.ca/problem/ccc18j2
+# https://dmoj.ca/problem/ccc16j2
